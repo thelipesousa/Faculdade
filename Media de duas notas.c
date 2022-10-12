3d94920e85196b7a9754dba0a0c9cd1a8b509a9f
Lipeeexxxx <101610971+Lipeeexxxx@users.noreply.github.com>
2022-10-12 11:13:49 -0300

    Add files via upload

---
+++ Media de duas notas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
+
+    int nota1, nota2, media;
+
+    printf("\nDigite a primeira nota: ");
+    scanf("%d",&nota1);
+    printf("\nDigite a segunda nota: ");
+    scanf("%d",&nota2);
+    media = (nota1 + nota2) / 2;
+    printf("\nA média das duas notas é: %d",media);
+    return 0;
+
+}
